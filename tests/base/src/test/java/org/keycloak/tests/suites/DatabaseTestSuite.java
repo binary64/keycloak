de7a4c28313502a8e8bf2b91f6568524f7405477
binary64 <1680627+binary64@users.noreply.github.com>
2025-10-19 15:56:03 +0100

    move helm template to root, for gh pages

--- tests/base/src/test/java/org/keycloak/tests/suites/DatabaseTestSuite.java
+++
@@ -1,9 +0,0 @@
-package org.keycloak.tests.suites;
-
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@SelectPackages({"org.keycloak.tests.admin", "org.keycloak.tests.db"})
-public class DatabaseTestSuite {
-}
